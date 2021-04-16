95ffcd52a5c69f88db1adf14dea10a7150b6402b
asya-khachatryan <asyakhachatryan1@gmail.com>
2021-04-16 15:48:24 +0400

    .

--- movietheater/MovieFilterer.java
+++ movietheater/MovieFilterer.java
@@ -1,8 +1,10 @@
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 public class MovieFilterer {
-    //Todo: Please take a look at builder pattern. I suggest using it for this filterer class to add any kinds of filter combinations and return fully filtered list later
+    //Todo: Please take a look at builder pattern. I suggest using it for this filterer
+    //class to add any kinds of filter combinations and return fully filtered list later
     //I also suggest taking a look at Stream interface as it is quite useful for filtering
 
     //Todo: Asya
@@ -20,7 +22,11 @@ public class MovieFilterer {
     //Todo: Asya
     //Find All Movie Sessions on Time of Day
     public List<MovieSession> filterByTimeOfDat(TimeOfDay timeOfDay, List<MovieSession> movieSessions){
-        return null;
+        movieSessions.stream().filter(movieSession -> {isBetween(movieSession.getLocalDateTime())})
     }
 
+
+    private boolean isBetween(LocalDateTime time, LocalDateTime start, LocalDateTime end) {
+        return time.isAfter(start) && time.isBefore(end);
+    }
 }
--- movietheater/TimeOfDay.java
+++ movietheater/TimeOfDay.java
@@ -3,16 +3,11 @@ import java.time.LocalDateTime;
 public enum TimeOfDay {
     MORNING, AFTERNOON, EVENING, NIGHT;
 
-    //TODO: Add a constructor which will receive start and end hours for each time of day and assign to corresponding instance variables
+    private int start;
+    private int end;
 
-    private String[] arr;
-    private int inter;
-    private int intra;
-
-    TimeOfDay(LocalDateTime start, LocalDateTime end) {
-        if (start.isBefore(12:00)){
-
-        }
-
+    TimeOfDay(int start, int end) {
+        this.start = start;
+        this.end = end;
     }
 }
